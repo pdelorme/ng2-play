2a9e511d7ff18e84a22e7606ca62776773fc1309
Olivier Combe <olivier.combe@gmail.com>
2015-10-18 08:49:47 +0200

    minimal example using ng2-translate

--- src/hello.ts
+++ src/hello.ts
@@ -1,14 +1,28 @@
-import {Component, bootstrap} from 'angular2/angular2';
+import {Component, bootstrap, Injectable} from 'angular2/angular2';
+import {TranslateService, TranslatePipe} from 'ng2-translate/ng2-translate';
+import {HTTP_PROVIDERS} from 'angular2/http';
 
+@Injectable()
 @Component({
     selector: 'hello-app',
+    providers: [TranslateService],
     template: `
         <h1>Hello, {{name}}!</h1>
-        Say hello to: <input [value]="name" (input)="name = $event.target.value">
-    `
+        Say {{ hello | translate }} to: <input [value]="name" (input)="name = $event.target.value">
+    `,
+    pipes: [TranslatePipe]
 })
 export class HelloApp {
     name: string = 'World';
+    hello: string = 'HELLO';
+
+    constructor(translate: TranslateService) {
+        translate.setDefault('en');
+        translate.setTranslation('en', {
+            'HELLO': 'hello'
+        });
+        translate.use('en');
+    }
 }
 
-bootstrap(HelloApp);
+bootstrap(HelloApp, [HTTP_PROVIDERS]);
